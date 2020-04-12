cc66549127734d5748aacb0ec75fe30e201aa1c0
탁동혁 <tdh8316@naver.com>
2020-04-12 13:55:24 +0900

    Remove duplicated directory

--- tello/__init__.py
+++
@@ -1,9 +0,0 @@
-from typing import Tuple
-
-version_tuple: Tuple[int, int, int] = (2020, 3, 8)
-
-version: str = "Tello.py {version}".format(
-    version=".".join(str(x) for x in version_tuple)
-)
-
-description: str = "Next-generation programming language for tello-edu"
