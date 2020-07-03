87163b1aef390e3cd7db59a70ff6043c8d962836
Donghyeok Tak <tdh8316@naver.com>
2020-07-03 22:06:49 +0900

    Update temp1.py

--- temp1.py
+++ temp1.py
@@ -6,12 +6,11 @@ my_tellos.append('0TQDFCAABBCCEE') # Replace with your Tello Serial Number
 
 with Tello(my_tellos) as fly: # Use Tello as a Context Manager to ensure safe landing in case of any errors 
     fly.takeoff() # Single command for all Tellos to take-off 
-    # fly.forward(50) # Single command for all Tellos to fly forward by 50cm with 
-    fly.forward(50) # Single command for all Tellos to fly forward by 50cm with 
-    fly.sync_these(): # Keep the following commands in-sync, even with different commands for each Tello 
+    fly.forward(50) # Single command for all Tellos to fly forward by 50cm 
     
-    fly.left(30, tello=1) # Tell just Tello1 to fly left 
-    fly.right(30, tello=2) # At the same time, Tello2 will fly right 
-    # fly.flip(direction='forward') # Flips are easy to perform via the Tello SDK 
-
-    fly.land()
+    with fly.sync_these(): # Keep the following commands in-sync, even with different commands for each Tello 
+        fly.left(30, tello=1) # Tell just Tello1 to fly left 
+        fly.right(30, tello=2) # At the same time, Tello2 will fly right 
+    
+    fly.flip(direction='forward') # Flips are easy to perform via the Tello SDK 
+    fly.land() 
